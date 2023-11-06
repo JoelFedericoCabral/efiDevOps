503f4d16ef9be8a5c83896917a35e667dfabacb4
Joel <f.cabral@itecriocuarto.org.ar>
2023-11-05 21:25:09 -0300

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 from app.config import Settings
 
@@ -11,12 +12,16 @@ app.config.from_object(Settings)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 # Endpoint
 @app.route('/')
 def index():
     return jsonify(message='Hola, bienvenido a la efi de DevOps')
 
+from app.views import users
+
+app.register_blueprint(users.bp, url_prefix='/users')
 
 
 
